0ede5e672359929e0527c0cfd80c40c29ad3be5d
unknown <you@example.com>
2025-11-15 01:41:43 +0300

    Recreate with more seasons

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from pandas.api.types import is_float_dtype
 
-from season_2024 import render_season_2024
+from seasons.season_2024 import render_season_2024
 
 st.set_page_config(page_title="F1 Manager Dashboard", layout="wide")
 
