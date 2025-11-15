be299c99f69ed2176806e7c58f6ad6e91d3a8e3e
unknown <you@example.com>
2025-11-15 12:20:19 +0300

    fix 13

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import pandas as pd
+
 from seasons.renderer import render_season
 from seasons.utils import load_season_data
 
+# ---------------------------------------------
+# Конфигурация страницы
+# ---------------------------------------------
 st.set_page_config(page_title="F1 Manager Dashboard", layout="wide")
 
+# ---------------------------------------------
+# Выбор сезона
+# ---------------------------------------------
 st.sidebar.title("Выбор сезона")
 season_choice = st.sidebar.selectbox("Сезон", ["2024", "2025"])
 
@@ -15,24 +22,29 @@ excel_files = {
 
 xls_path = excel_files[season_choice]
 
-# ❗ передаём ГОД отдельно
+# Загружаем сезон
 season_data = load_season_data(xls_path)
 
-# список гонок
-gp_list = season_data["gp_list"]
-race_name = st.sidebar.selectbox(
-    "Выбор Гран-при",
-    list(gp_list.values())
-)
+# ---------------------------------------------
+# Выбор Гран-при
+# ---------------------------------------------
+gp_list = season_data.get("gp_list", {})
 
-# получаем код обратно
+if not gp_list:
+    st.error("Не удалось загрузить список Гран-при. Проверь файл Excel.")
+    st.stop()
+
+# список названий ГП
+race_name = st.sidebar.selectbox("Выбор Гран-при", list(gp_list.values()))
+
+# код ГП по названию
 race_code = next(code for code, name in gp_list.items() if name == race_name)
 
-
-gp_code = gp_list.set_index("Название").loc[race_choice, "Код"]
-
+# ---------------------------------------------
+# РЕНДЕР ПОДСТРАНИЦ
+# ---------------------------------------------
 render_season(
     season_name=season_choice,
-    race_code=gp_code,
+    race_code=race_code,
     data=season_data
 )
